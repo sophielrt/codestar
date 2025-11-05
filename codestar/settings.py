4731c0dda13672a39a8e28dff41304c2252efa84
sophielrt <sophielouiserose@icloud.com>
2025-11-05 12:15:55 +0000

    added SECRET_KEY to secure database

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ku4&lx3udti@zewu(aw!p(npv^&+e5vzl=5*3$%xfbjpftg2cb'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
